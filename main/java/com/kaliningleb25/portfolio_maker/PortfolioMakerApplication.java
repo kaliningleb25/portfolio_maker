235b8f26c0544b14c40c09d77d1a672708105cf2
glebkalinin <kaliningleb25@gmail.com>
2018-06-24 21:26:59 +0300

    master: initialize commit

---
+++ main/java/com/kaliningleb25/portfolio_maker/PortfolioMakerApplication.java
@@ -0,0 +1,12 @@
+package com.kaliningleb25.portfolio_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioMakerApplication.class, args);
+	}
+}
---
+++ test/java/com/kaliningleb25/portfolio_maker/PortfolioMakerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaliningleb25.portfolio_maker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortfolioMakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
