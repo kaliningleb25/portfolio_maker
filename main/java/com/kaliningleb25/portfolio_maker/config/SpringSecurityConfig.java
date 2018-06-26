202eb672a89d1139642a87479b19759c537eeb6e
glebkalinin <kaliningleb25@gmail.com>
2018-06-26 19:44:05 +0300

    user_features: added security support

---
+++ main/java/com/kaliningleb25/portfolio_maker/config/SpringSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.kaliningleb25.portfolio_maker.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    // роль admin всегда есть доступ к /admin/**
+    // роль user всегда есть доступ к /user/**
+    // Наш кастомный "403 access denied" обработчик
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/about").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    // создаем пользоватлелей, admin и user
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER") // TODO: remove noop
+                .and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");  // TODO: remove noop
+    }
+}
---
+++ main/java/com/kaliningleb25/portfolio_maker/controller/MainController.java
@@ -0,0 +1,50 @@
+package com.kaliningleb25.portfolio_maker.controller;
+
+import com.kaliningleb25.portfolio_maker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+//        model.addAttribute("user", userService.findUser(1L));
+
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "/user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
+
+
+
+
+}
